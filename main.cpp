8e0a5a8af2b25e3853019a576eb0070026ea8e43
Archy User <archy@TA-TKY-A-99.selini.tech>
2024-10-24 17:07:42 +0000

    wip2

--- main.cpp
+++ main.cpp
@@ -5,19 +5,23 @@
 
 #include "maps_impl.h"
 
-#define n_elems 1'000'000
+#define n_elems 10'000
 #define max_num 100'000
-#define n_capacity 5
+#define n_capacity 4096
+
+/* 64B, 512B, 1KB, 2KB, 4KB object - random data - generate objects prior to profiling */
+#define obj_size 64
 
 using number_type = uint64_t;
-using obj_type = uint64_t;
+
+using obj_type = std::array<char, obj_size>;
 
 static size_t pos_insert = 0;
 static size_t pos_delete = 0;
 
-void batch_insert(auto& map_impl, const std::array<number_type, n_elems>& v, size_t n) {
+void batch_insert(auto& map_impl, const std::array<number_type, n_elems>& keys, const std::array<obj_type, n_elems>& values, size_t n) {
     for (size_t i = pos_insert; i < pos_insert + n; i++) {
-        map_impl.insert(v[i % n_elems], number_type());
+        map_impl.insert(keys[i % n_elems], values[i % n_elems]);
     }
     pos_insert += n;
 }
@@ -31,41 +35,66 @@ void batch_remove(auto& map_impl, const std::array<number_type, n_elems>& v, siz
 
 int main(int argc, char* argv[]) {
     
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+ 
+    std::normal_distribution<double> d{0.0, max_num / 4.0};
+ 
+    auto random_int = [&d, &gen]{ return number_type(abs(std::round(d(gen)))); };
+
+
     /* Generate random numbers */
-    std::array<number_type, n_elems> v;
+    std::array<number_type, n_elems> keys;
     for (size_t i = 0; i < n_elems; i++) {
-        number_type n = rand() % max_num;
-        v[i] = n;
+        number_type n = random_int() % max_num;
+        keys[i] = n;
     }
+
+    /* Construct values */
+    std::array<obj_type, n_elems> values;
+    for (size_t i = 0; i < n_elems; i++) {
+        values[i] = obj_type();
+    }
+
     std::ofstream res;
-    res.open("results.csv", std::ios::out | std::ios::trunc);
+    res.open("../results.csv", std::ios::out | std::ios::trunc);
     res << "epoch" << "," << "time_taken" << '\n';
 
     timespec ts_monotonic_start;
     timespec ts_monotonic_end;
 
-    unordered_map_wrapper<number_type, obj_type, n_capacity> m;
-    for (size_t epoch_no = 0; epoch_no < 10; epoch_no++) {
+    ordered_map_wrapper<number_type, obj_type, n_capacity> map;
+    for (size_t epoch_no = 0; epoch_no < 20; epoch_no++) {
         /* Measure time taken to do several batch inserts and batch deletes from the map */
+        std::cout << "Epoch: " << epoch_no << '\n';
         clock_gettime(CLOCK_MONOTONIC, &ts_monotonic_start);
-        for (size_t i = 0; i < 1'000'000; i++) {
-            batch_insert(m, v, 50);
-            // for (auto it = m.map_internal.begin(); it != m.map_internal.end(); it++) {
-            //     std::cout << it->first << " " << it->second << std::endl;
-            // }
-            // std::cout << std::endl;
-            batch_remove(m, v, 10);
-            // for (auto it = m.map_internal.begin(); it != m.map_internal.end(); it++) {
-            //     std::cout << it->first << " " << it->second << std::endl;
-            // }
-            // std::cout << std::endl;
+        for (size_t i = 0; i < 1'000; i++) {        
+            /* One out of every 100 repetitions should insert 3000 elements */
+            /* Remaining should insert 20 and delete 10 */
+            if (i % 100 == 0) {
+                batch_insert(map, keys, values, 3000);
+            } else {
+                batch_insert(map, keys, values, 20);
+                // for (auto it = map.map_internal.begin(); it != map.map_internal.end(); it++) {
+                    // std::cout << it->first << std::endl;
+                // }
+                // std::cout << std::endl;
+                batch_remove(map, keys, 10);
+                // for (auto it = map.map_internal.begin(); it != map.map_internal.end(); it++) {
+                    // std::cout << it->first << std::endl;
+                // }
+                // std::cout << std::endl;
+            }
         }
         clock_gettime(CLOCK_MONOTONIC, &ts_monotonic_end);
+        /* Cast to int64 before multiplication */
         std::uint64_t time_taken = (ts_monotonic_end.tv_sec - ts_monotonic_start.tv_sec) * 1e9 + (ts_monotonic_end.tv_nsec - ts_monotonic_start.tv_nsec);
-        m.clear();
+        map.clear();
 
         /* Append results to csv */
         res << epoch_no << "," << time_taken << '\n';
     }
+
+    res.close();
 
 }
--- maps_impl.h
+++ maps_impl.h
@@ -1,7 +1,10 @@
 #include <optional>
 #include <map>
+#include <queue>
 #include <unordered_map>
-
+#include <absl/container/flat_hash_map.h>
+#include <absl/container/btree_map.h>
+#include <boost/container/flat_map.hpp>
 
 template<typename T, typename U, typename V, size_t CAPACITY>
 struct map_base
@@ -23,24 +26,19 @@ template<typename U, typename V, size_t CAPACITY>
 struct unordered_map_wrapper : public map_base<unordered_map_wrapper<U, V, CAPACITY>, U, V, CAPACITY>
 {
     std::unordered_map<U, V> map_internal;
-    size_t occupancy = 0;
+    U largest = 0;
 
     void insert(const U& key, const V& value) {
-        if (occupancy == CAPACITY) {
+        if (map_internal.size() == CAPACITY) {
             /* Decide whether to insert */
-            U max_key_val = 0;
-            for (auto it = map_internal.begin(); it != map_internal.end(); it++) {
-                max_key_val = std::max(max_key_val, it->first);
-            }
-            if (key < max_key_val) {
-                /* Delete highest element if necessary */
-                map_internal.erase(map_internal.find(max_key_val));
+            if (key < largest) {
                 map_internal.emplace(key, value);
+                this->remove(largest);
             }
             
         } else {
             map_internal.emplace(key, value);
-            occupancy++;  
+            this->largest = std::max(this->largest, key);
         }
     }
 
@@ -48,23 +46,73 @@ struct unordered_map_wrapper : public map_base<unordered_map_wrapper<U, V, CAPAC
         auto it = map_internal.find(key);
         if (it != map_internal.end()) {
             map_internal.erase(it);
-            occupancy--;
+            if (key == this->largest) {
+                this->largest = 0;
+                if (map_internal.size() != 0) {
+                    for (auto it = map_internal.begin(); it != map_internal.end(); it++) {
+                        this->largest = std::max(this->largest, it->first);
+                    }
+                }
+            }
         }
+
     }
 
     void clear() {
-        map_internal.clear();   
+        map_internal.clear();
     }
 };
+
+template<typename U, typename V, size_t CAPACITY>
+struct flat_hash_map_wrapper : public map_base<flat_hash_map_wrapper<U, V, CAPACITY>, U, V, CAPACITY>
+{
+    absl::flat_hash_map<U, V> map_internal;
+    U largest = 0;
+
+    void insert(const U& key, const V& value) {
+        if (map_internal.size() == CAPACITY) {
+            /* Decide whether to insert */
+            if (key < largest) {
+                map_internal.emplace(key, value);
+                this->remove(largest);
+            }
+            
+        } else {
+            map_internal.emplace(key, value);
+            this->largest = std::max(this->largest, key);
+        }
+    }
+
+    void remove(const U& key) {
+        auto it = map_internal.find(key);
+        if (it != map_internal.end()) {
+            map_internal.erase(it);
+            if (key == this->largest) {
+                this->largest = 0;
+                if (map_internal.size() != 0) {
+                    for (auto it = map_internal.begin(); it != map_internal.end(); it++) {
+                        this->largest = std::max(this->largest, it->first);
+                    }
+                }
+            }
+        }
+
+    }
+
+    void clear() {
+        map_internal.clear();
+    }
+};
+
+/* Ordered containers */
 
 template<typename U, typename V, size_t CAPACITY>
 struct ordered_map_wrapper : public map_base<ordered_map_wrapper<U, V, CAPACITY>, U, V, CAPACITY>
 {
     std::map<U, V> map_internal;
-    size_t occupancy = 0;
 
     void insert(const U& key, const V& value) {
-        if (occupancy == CAPACITY) {
+        if (map_internal.size() == CAPACITY) {
             auto it = map_internal.upper_bound(key);
             if (it != map_internal.end()) {
                 map_internal.emplace(key, value);
@@ -72,7 +120,6 @@ struct ordered_map_wrapper : public map_base<ordered_map_wrapper<U, V, CAPACITY>
             }
         } else {
             map_internal.emplace(key, value);
-            occupancy++;
         }
     }
 
@@ -80,11 +127,68 @@ struct ordered_map_wrapper : public map_base<ordered_map_wrapper<U, V, CAPACITY>
         auto it = map_internal.find(key);
         if (it != map_internal.end()) {
             map_internal.erase(it);
-            occupancy--;
         }
     }
 
     void clear() {
-        map_internal.clear();   
+        map_internal.clear(); 
+    }
+};
+
+template<typename U, typename V, size_t CAPACITY>
+struct flat_ordered_map_wrapper : public map_base<flat_ordered_map_wrapper<U, V, CAPACITY>, U, V, CAPACITY>
+{
+    boost::container::flat_map<U, V> map_internal;
+
+    void insert(const U& key, const V& value) {
+        if (map_internal.size() == CAPACITY) {
+            auto it = map_internal.upper_bound(key);
+            if (it != map_internal.end()) {
+                map_internal.emplace(key, value);
+                map_internal.erase(std::next(map_internal.rbegin()).base());
+            }
+        } else {
+            map_internal.emplace(key, value);
+        }
+    }
+
+    void remove(const U& key) {
+        auto it = map_internal.find(key);
+        if (it != map_internal.end()) {
+            map_internal.erase(it);
+        }
+    }
+
+    void clear() {
+        map_internal.clear(); 
+    }
+};
+
+template<typename U, typename V, size_t CAPACITY>
+struct btree_ordered_map_wrapper : public map_base<btree_ordered_map_wrapper<U, V, CAPACITY>, U, V, CAPACITY>
+{
+    absl::btree_map<U, V> map_internal;
+
+    void insert(const U& key, const V& value) {
+        if (map_internal.size() == CAPACITY) {
+            auto it = map_internal.upper_bound(key);
+            if (it != map_internal.end()) {
+                map_internal.emplace(key, value);
+                map_internal.erase(std::next(map_internal.rbegin()).base());
+            }
+        } else {
+            map_internal.emplace(key, value);
+        }
+    }
+
+    void remove(const U& key) {
+        auto it = map_internal.find(key);
+        if (it != map_internal.end()) {
+            map_internal.erase(it);
+        }
+    }
+
+    void clear() {
+        map_internal.clear(); 
     }
 };
